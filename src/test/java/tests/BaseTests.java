5bfe1e5c29916c9fa07a8dd72b176f92f7a7f72c
Ulikk26 <ulikk@bk.ru>
2025-01-08 13:26:50 +0300

    added chrome

--- src/test/java/tests/BaseTests.java
+++ src/test/java/tests/BaseTests.java
@@ -3,16 +3,21 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import steps.CreateAccountSteps;
 import steps.SingInSteps;
 import utils.PropertyReader;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
+@Log4j2
 public class BaseTests {
 
     CreateAccountSteps createAccountSteps;
@@ -23,23 +28,36 @@ public class BaseTests {
     String password = System.getProperty("password", PropertyReader.getProperty("password"));
     String wrongPassword = System.getProperty("wrongPassword", PropertyReader.getProperty("wrongPassword"));
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp() {
-        FirefoxOptions options = new FirefoxOptions();
- //       ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--headless");
-        options.addArguments("--allow-insecure-localhost");
-        options.addArguments("--disable-web-security");
-        options.addArguments("--ignore-certificate-errors");
-        options.addArguments("--unsafely-treat-insecure-origin-as-secure=http://prestashop.qatestlab.com.ua");
-        Configuration.browserCapabilities = options;
- //       Configuration.browser = "chrome";
-        Configuration.browser = "firefox";
-        Configuration.headless = false;
-        Configuration.timeout = 10000;
-        Configuration.clickViaJs = true;
-        Configuration.baseUrl = baseUrl;
+    public void setUp(@Optional("fireFox") String browser) {
+        log.info("Open browser {}", browser);
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--headless");
+            options.addArguments("--allow-insecure-localhost");
+            options.addArguments("--disable-web-security");
+            options.addArguments("--ignore-certificate-errors");
+            options.addArguments("--unsafely-treat-insecure-origin-as-secure=http://prestashop.qatestlab.com.ua");
+            Configuration.browserCapabilities = options;
+            Configuration.browser = "chrome";
+            Configuration.headless = false;
+            Configuration.timeout = 10000;
+            Configuration.clickViaJs = true;
+            Configuration.baseUrl = baseUrl;
+        } else if (browser.equalsIgnoreCase("fireFox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--start-maximized");
+       //     options.addArguments("--headless");
+            Configuration.browserCapabilities = options;
+            Configuration.browser = "firefox";
+            Configuration.headless = false;
+            Configuration.timeout = 10000;
+            Configuration.clickViaJs = true;
+            Configuration.baseUrl = baseUrl;
+        }
+
 
         createAccountSteps = new CreateAccountSteps();
         singInSteps = new SingInSteps();
